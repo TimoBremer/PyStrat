34ab8c45d864f44fa1ffa4f6aa12515fb901067e
TimoBremer <34343137+TimoBremer@users.noreply.github.com>
2022-01-10 16:05:55 +0100

    Delete anleitung_bundle.py

--- anleitung_bundle.py
+++
@@ -1,7 +0,0 @@
-# läuft unten im Terminal von VS-Code.
-# ...Programm-Datei muss im gleichen Pfad sein
-
-# bei Wiederholung müssen die Dateien "dist" und "build" zuvor gelöscht werden
-    # geht nur, wenn die gerade nicht ausgeführt werden
-
-pyinstaller --noconsole --add-data 'C:\Users\timob\OneDrive\Desktop\strati\gui_strati.ui;.' --add-data 'C:\Users\timob\OneDrive\Desktop\strati\gui_tab_apply.ui;.' --add-data 'C:\Users\timob\OneDrive\Desktop\strati\gui_tab_save.ui;.' aufruf_gui_strati.py
